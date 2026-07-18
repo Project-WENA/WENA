62fe14a73a496c65f2655f5c554155a75483c88b
Project-WENA <reachout@wena.co.in>
2026-07-19 01:51:35 +0530

    Create get-continue-learning.js

---
+++ api/get-continue-learning.js
@@ -0,0 +1,16 @@
+const { requireSession } = require('../lib/session');
+const { getFile } = require('../lib/github');
+
+module.exports = async (req, res) => {
+  if (!requireSession(req)) {
+    res.status(401).json({ error: 'Not logged in' });
+    return;
+  }
+  try {
+    const file = await getFile('content/continue-learning.json');
+    const data = file ? JSON.parse(file.content) : { left: { youtube_url: '' }, right: { youtube_url: '' } };
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
