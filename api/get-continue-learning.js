6f9f0d530a2984d5df9741f8673ffdee09aa5afd
Project-WENA <reachout@wena.co.in>
2026-07-19 15:34:01 +0530

    Update get-continue-learning.js

--- api/get-continue-learning.js
+++ api/get-continue-learning.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
   }
   try {
     const file = await getFile('content/continue-learning.json');
-    const data = file ? JSON.parse(file.content) : { left: { youtube_url: '' }, right: { youtube_url: '' } };
+    const data = file ? JSON.parse(file.content) : { left: { youtube_url: '' }, center: { youtube_url: '' }, right: { youtube_url: '' } };
     res.status(200).json(data);
   } catch (e) {
     res.status(500).json({ error: e.message });
